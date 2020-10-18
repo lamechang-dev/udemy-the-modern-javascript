42d15b5590014fbb9634ced8eb514a157ba6bb4e
lamechang-van <yoshimoto2107@gnail.com>
2020-10-18 10:40:54 +0900

    [update]38. Array Basics

---
+++ arrays/todo.js
@@ -0,0 +1,17 @@
+const notes = [
+    'note 1',
+    'note 2',
+    'note 3'
+]
+
+console.log(notes);
+console.log(notes.length);
+console.log(notes[notes.length - 1]);
+
+const todos = [
+    'wash your hands',
+    'take the shower',
+    'watch TV',
+];
+
+console.log(`Last Todo: ${todos[todos.length - 1]}`);
