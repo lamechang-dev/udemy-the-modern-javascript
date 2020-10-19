dbad148cdb14ac5c668fc0b96a57763aaa7b309e
lamechang-van <yoshimoto2107@gnail.com>
2020-10-20 07:38:32 +0900

    [update]46. Improve Our Expense Tracker

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,15 +1,26 @@
 const notes = [
     {
         title: 'my next trip',
-        body: "I'd like to go to vancouver"
+        body: "I'd like to go to vancouver",
+        page: 2
     },
     {
         title: 'my favorite food',
-        body: 'chicken'
+        body: 'chicken',
+        page: 1
     },
     {
         title: 'my favorite sweets',
-        body: 'sweet potato'
+        body: 'sweet potato',
+        page: 3
+    }
+];
+
+const alteredNotes = [
+    {
+        title: 'my next trip',
+        body: "I'd like to go to vancouver",
+        page: 2
     }
 ];
 
@@ -20,10 +31,26 @@ const notes = [
 //     return index !== -1 ? `Yay! I could find ${notes[index].title} !` : "Oops, the target note doesn't available!";
 // }
 
-const findNote = (notes, noteTitle) => {
-    return notes.find((note) => {
-        return note.title.toLowerCase() === noteTitle.toLowerCase();
+// const findNote = (notes, noteTitle) => {
+//     return notes.find((note) => {
+//         return note.title.toLowerCase() === noteTitle.toLowerCase();
+//     })
+// }
+
+// const filterNotes = (notes, noteTitle) => {
+//     return notes.filter((note, index) => {
+//         return note.title === noteTitle;
+//     })
+// }
+
+const sortNotesByPageNum = (notes) => {
+    return notes.sort((noteA, noteB) => {
+        return noteA.page - noteB.page
     })
 }
 
-console.log(findNote(notes, 'my favorite sweets'))
+const sortedNotesByPageNum = sortNotesByPageNum(notes);
+console.log(sortedNotesByPageNum);
+
+// const filteredNotes = filterNotes(notes, 'my next trip');
+// console.log(filteredNotes);
