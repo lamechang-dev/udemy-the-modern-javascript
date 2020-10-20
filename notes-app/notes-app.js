5ac83d31c7c1daf7be530fe67801f1f84444a34f
lamechang-van <yoshimoto2107@gnail.com>
2020-10-20 21:22:34 +0900

    [update]50. DOM Manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,7 @@
-console.log("this is from the other javascript file!!");
+// const pElement = document.querySelector('p');
+// pElement.remove()
+
+const ps = document.querySelectorAll("p");
+ps.forEach((p) => {
+    console.log(p.textContent = "This is dummy text");
+})
